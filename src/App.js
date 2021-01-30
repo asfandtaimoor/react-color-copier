7b78f60105ad75535501276d71233b68d245c4cd
Taimoor Salyhal <asfandtaimoor786@gmail.com>
2021-01-30 09:00:26 +0500

    Last Version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+// Tostify
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+toast.configure();
+// Components
 
 function App() {
+  const [colorNames] = useState([
+    "#fad390",
+    "#f8c291",
+    "#6a89cc",
+    "#82ccdd",
+    "#b8e994",
+    "#f6b93b",
+    "#e55039",
+    "#4a69bd",
+    "#60a3bc",
+    "#78e08f",
+    "#fa983a",
+    "#eb2f06",
+    "#1e3799",
+    "#3c6382",
+    "#38ada9",
+    "#e58e26",
+    "#b71540",
+    "#0c2461",
+    "#0a3d62",
+    "#079992",
+  ]);
+
+  // Copy color
+  const copyColor = (e) => {
+    let color = e.target.innerText;
+    document.execCommand(color); //copy to clipboard
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(color);
+      navigator.clipboard.readText();
+      toast.success(`You have copied ${color} value successfully`, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section>
+      {colorNames.map((color) => (
+        <div
+          key={color}
+          className="color__block"
+          style={{ background: color }}
+          onClick={copyColor}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="color--text">
+            <p>{color}</p>
+          </div>
+        </div>
+      ))}
+    </section>
   );
 }
 
